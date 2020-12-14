6ab8eb3c4e97c0e528d3672f95cf72600031d6f3
junguanghuang@gmail.com <junguanghuang@gmail.com>
2020-12-14 14:37:10 +0800

    '12-14'

--- 20201109/49.js
+++ 20201109/49.js
@@ -12,6 +12,17 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+var groupAnagrams = function(strs) {
+  const map = new Map()
+  for(let str of strs) {
+    let key = str.split("").sort().join("")
+    let list = map.get(key) ? map.get(key) : []
+    list.push(str)
+    map.set(key, list)
+  }
+  return Array.from(map.values())
+};
+
 
 var groupAnagrams = function(strs) {
   const map = new Map();
