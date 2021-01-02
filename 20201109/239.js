3a8ff63ae6746953b16bbc3ec0198dc9eeaa5523
junguanghuang@gmail.com <junguanghuang@gmail.com>
2021-01-02 23:08:18 +0800

    '21-01-02'

--- 20201109/239.js
+++ 20201109/239.js
@@ -36,17 +36,23 @@
  * @param {number} k
  * @return {number[]}
  */
-var maxSlidingWindow = function(nums, k) {
-  const deque=[]; //存放单调队列的下标
-  const  result=[];
-  for(let i=0;i<nums.length;i++){
-    if(i-deque[0]>=k) deque.shift(); //在滑动窗口之外的直接从队头删掉
-    while(nums[deque[deque.length-1]]<=nums[i]){
+// 核心思路：维护一个单调队列，队头元素到队尾元素依次递减，然后最大值就是队列中的第一个元素
+// 合法性判断：滑动窗口外的元素删掉
+// 维护单调队列：如果新加进来的数大于等于队尾元素，则依次将队尾元素删除，始终维护队头元素是最大值
+// 当满足滑动窗口区间范围时，将队头元素推入结果数组中。
+// 时间复杂度为O(N),空间复杂度为O(N)
+
+var maxSlidingWindow = function (nums, k) {
+  const deque = []; //存放单调队列的下标
+  const result = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (i - deque[0] >= k) deque.shift(); //在滑动窗口之外的直接从队头删掉
+    while (nums[deque[deque.length - 1]] <= nums[i]) {
       deque.pop();  //如果新加进来的数比单调队列中原来的数都要大，则直接弹出队列中的其他数
     }
     deque.push(i);
     //数组下标从0开始，k=3时 ，下标为0，1，2的数组元素构成一个滑动窗口，所以条件为i>=k-1就可以将答案存入res中
-    if(i>=k-1) {
+    if (i >= k - 1) {
       result.push(nums[deque[0]]);
     }
   }
